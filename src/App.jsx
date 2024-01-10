fb66e9a34cb8a5ce25d55777a54edbce5d2d6576
Jiran <paiveryhappy@gmail.com>
2024-01-10 15:44:17 +0700

    fix: fix a bug

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,15 @@ import "./App.css";
 function App() {
   const [products, setProducts] = useState([]);
 
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("https://dummyjson.com/products");
+      setProducts(response.data.products);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
   useEffect(() => {
-    const fetchData = () => {
-      try {
-        const response = await axios.get("https://dummyjson.com/products");
-        setProducts(response.data.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
     fetchData();
   }, []);
 
@@ -21,14 +21,18 @@ function App() {
     <div className="app-container">
       <h1>Product List</h1>
       <div className="product-list">
-        {products.map((product) => (
-          <div className="product-card" key={product.id}>
-            <h2>{product.title}</h2>
-            <p>Description: {product.description}</p>
-            <p>Price: ${product.price}</p>
-            <img src={product.picture} alt={product.title} />
-          </div>
-        ))}
+        {products.map((product) => {
+          return (
+            <div className="product-list" key={product.id}>
+              <div className="product-card">
+                <h2>{product.title}</h2>
+                <p>Description: {product.description}</p>
+                <p>Price: ${product.price}</p>
+                <img src={product.images[0]} alt={product.title} />
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
